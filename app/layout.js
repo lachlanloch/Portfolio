c033428d4d13a0190899fe904a389df36a914cb3
lachlanloch <lachlansim123@gmail.com>
2024-10-01 12:44:57 +1300

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,5 @@
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "../app/globals.css"
+import Head from "./head";
 
 export const metadata = {
   title: "Create Next App",
@@ -20,9 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+
+      <Head/>
+      <body>
         {children}
       </body>
     </html>
